7df04d50ed9fab84319b78450e04e26f065230f1
tarek <tarekhossian285@gmail.com>
2026-09-02 01:33:17 +0600

    feat: add join_request table

--- database/schema.sql
+++ database/schema.sql
@@ -29,3 +29,18 @@ create table members(
     ON UPDATE CASCADE
 
 );
+create table join_requests(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT ,
+    mess_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    status ENUM('pending','approved', 'rejected') NOT NULL DEFAULT 'pending',
+    requested_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (mess_id) REFERENCES messes(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE   
+    ON UPDATE CASCADE
+
+);
