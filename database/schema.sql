239608a709e4b4db354dc57753b6944fbea0a553
tarek <tarekhossian285@gmail.com>
2026-09-02 06:50:33 +0600

    feat: add meal_orders table

--- database/schema.sql
+++ database/schema.sql
@@ -49,11 +49,26 @@ create table meal_sessions(
     mess_id BIGINT NOT NULL,
     meal_type ENUM('breakfast', 'lunch', 'dinner') NOT NULL ,
     meal_date DATE NOT NULL,
-    cooked_start_time TIME NOT NULL,
+    cooked_start_time TIME NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     UNIQUE KEY unique_meal_session (mess_id, meal_type, meal_date),
     FOREIGN KEY (mess_id) REFERENCES messes(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
+    
 
 );
+
+create table meal_orders(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    meal_session_id BIGINT NOT NULL,
+    member_id BIGINT NOT NULL,
+    quantity INT NOT NULL check (quantity > 0),
+    ordered_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (meal_session_id) REFERENCES meal_sessions(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (member_id) REFERENCES members(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE   
+)
