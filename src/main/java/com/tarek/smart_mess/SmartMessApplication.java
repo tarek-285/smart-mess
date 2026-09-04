c762bfbd8eb94d8714f857ea75383a5f986b984c
tarek <tarekhossian285@gmail.com>
2026-09-04 06:43:58 +0600

    chore configure spring boot and mysql

---
+++ src/main/java/com/tarek/smart_mess/SmartMessApplication.java
@@ -0,0 +1,13 @@
+package com.tarek.smart_mess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarek/smart_mess/SmartMessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarek.smart_mess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
